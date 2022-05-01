ba46d1b333f211f1d32092b6b329ea80c79845f6
Shah Rahim <sr278@njit.edu>
2022-04-30 23:52:34 -0400

    Add some stuff

--- src/main/java/net/njit/apache/spark/trainer/util/FileSanitizer.java
+++ src/main/java/net/njit/apache/spark/trainer/util/FileSanitizer.java
@@ -1,5 +1,11 @@
 package net.njit.apache.spark.trainer.util;
 
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -31,8 +37,24 @@ public class FileSanitizer {
                 path.getFileName().toString());
         Path sPath = Paths.get(path.getParent().toString() + "/" + sanitizedFilename);
         Files.write(sPath, content.getBytes(charset));
+        writeSanitizedToS3(sanitizedFilename, sPath.toAbsolutePath().toString());
         return sPath.toAbsolutePath().toString();
 
+    }
 
+    private static void writeSanitizedToS3(String fileName, String path) {
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withRegion(Regions.US_WEST_2)
+                .build();
+
+        String bucketName = System.getenv("BUCKET_NAME");
+
+        if(bucketName == null || bucketName.isEmpty()) {
+            throw new RuntimeException("BUCKET_NAME environment must be set");
+        }
+
+        s3client.putObject(bucketName, fileName, new File(path));
     }
 }
